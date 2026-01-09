a972cdb58f784152f24362acb28fbab5101ec266
Osif ungar <osif.e.u@gmail.com>
2026-01-09 18:50:52 +0200

    Delete backend directory

--- backend/app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-﻿from fastapi import APIRouter
-from app.api.v1.endpoints import auth
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-
-# TODO: Add predictions router later
-# api_router.include_router(predictions.router, prefix="/predictions", tags=["predictions"])
