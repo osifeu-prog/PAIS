89bff10be2cc929540fcac7f6e5de044e157cb03
Osif ungar <osif.e.u@gmail.com>
2026-01-09 20:29:47 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,16 +1,13 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import relationship
 from app.db.database import Base
 
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
     points = Column(Integer, default=0)
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
-    
-    predictions = relationship("Prediction", back_populates="user")
