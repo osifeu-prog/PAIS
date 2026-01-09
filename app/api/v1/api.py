f62dc9d8a7f789f9ce26cd15bd30b7d96b18b432
Osif ungar <osif.e.u@gmail.com>
2026-01-09 18:51:00 +0200

    Delete app directory

--- app/api/v1/api.py
+++
@@ -1,6 +0,0 @@
-﻿from fastapi import APIRouter
-from app.api.v1.endpoints import auth, predictions
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(predictions.router, prefix="/predictions", tags=["predictions"])
