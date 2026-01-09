334ce5389d2cc1d5b8220307a6d2f4732e80d9e5
Osif ungar <osif.e.u@gmail.com>
2026-01-09 20:30:25 +0200

    Update token.py

--- schemas/token.py
+++ schemas/token.py
@@ -3,7 +3,3 @@ from pydantic import BaseModel
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-class TokenData(BaseModel):
-    username: str | None = None
-    user_id: int | None = None
