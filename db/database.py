ac5cdfc758ef392c7baa9e1d2fd898d514e1db2e
Osif ungar <osif.e.u@gmail.com>
2026-01-09 20:29:32 +0200

    Update database.py

--- db/database.py
+++ db/database.py
@@ -5,7 +5,6 @@ from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
