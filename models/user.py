01f0dea8ceed384f3d37eebf2d51d698a889aaef
Osif ungar <osif.e.u@gmail.com>
2026-01-09 19:13:47 +0200

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    points = Column(Integer, default=0)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+    
+    predictions = relationship("Prediction", back_populates="user")
